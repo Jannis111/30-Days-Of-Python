6ab5470d0776e24c757bfcfb59479a2719cf5715
Jannis Geerts <JA.Geerts@student.han.nl>
2025-10-04 18:22:14 +0200

    day1
    Printed the basic data types in python.

---
+++ day_1/helloworld.py
@@ -0,0 +1,42 @@
+import math
+# exercise level 2
+# question 2
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# question 3
+print("Jannis")
+print("Geerts")
+print("Netherlands")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Jannis"))
+print(type("Geert"))
+print(type("Netherlands"))
+
+#exercise level 3
+# question 1
+print(type(True))
+print(type({'name':'Asabeneh',
+            'name':'Jannis',
+            'surname':'Geerts'
+            }))
+print(type(('tuple1', 'tuple2', "tuple1")))
+print(type(({1,2,3,'set'})))
+print(type(4 == 4))
+
+
+# question 2
+# d(p,q) = √((p1 - q1)^2) + ((p2 - q2)^2) 
+# (2, 3) and (10, 8)
+print(math.sqrt(((10 - 2)**2) + ((8 - 3)**2)))
